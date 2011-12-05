bdfc2b9244a3eef3d6179e132c8997ba52b721b4
David arcoleo <david.arcoleo@kontagent.com>
2011-12-05 12:04:51 -0800

    add files

---
+++ emacs/dot_emacs.d/custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-customization-version-id 213 t)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(default-frame-alist (quote ((menu-bar-lines . 1) (foreground-color . "Black") (background-color . "White") (cursor-type . box) (cursor-color . "Red") (vertical-scroll-bars . right) (internal-border-width . 0) (left-fringe . 1) (right-fringe) (fringe) (tool-bar-lines . 0))))
+ '(global-hl-line-mode t)
+ '(global-linum-mode t)
+ '(inverse-video t)
+ '(ns-tool-bar-display-mode nil t)
+ '(ns-tool-bar-size-mode nil t)
+ '(visual-line-mode nil t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(text-mode-default ((t (:inherit autoface-default :stipple nil :background "#000000" :foreground "#ffffff" :strike-through nil :underline nil :slant normal :weight normal :height 130 :width normal :family "Lucida Grande")))))
